bde98c341bcdeba0c5ef909ef6abf9a2c98d3e2f
xandregc <alexandre.gcosta@gmail.com>
2014-04-22 10:44:07 -0300

    Atualizando módulo pilha

--- fila/fila.py
+++ fila/fila.py
@@ -16,4 +16,42 @@ class No:
 class Fila:
     def __init__(self):
         self.tamanho = 0
-        self.topo = None
+        self.frente = None
+        self.traz = None
+
+    def insereFila(self, info):
+        no = No(info)
+        if (self.traz!=None):
+            self.traz.link = no
+            self.traz = no
+        else:
+            self.traz = no
+            self.frente = no
+
+    def removerFila(self):
+        if (self.frente==None):
+            return None
+        else:
+            removendo = self.frente
+            self.frente = removendo.link
+            if (self.frente==None):
+                self.traz = None
+            return removendo.info
+
+# Funcao principal
+def main():
+    fila = Fila()
+
+    fila.insereFila(10)
+    fila.insereFila(20)
+    fila.insereFila(30)
+    
+    imprime = fila.frente
+
+    while (fila.frente!=None):
+        print fila.removerFila()
+
+    return
+
+if __name__ == "__main__":
+    main()
